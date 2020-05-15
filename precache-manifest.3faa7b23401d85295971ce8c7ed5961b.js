6a4ff8d0e5ecd703ad16644dd45acdc2d450b6ba
gusma <gustavo.malamud@globant.com>
2020-05-15 15:50:42 -0300

    Updates

---
+++ precache-manifest.3faa7b23401d85295971ce8c7ed5961b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec857647f63031f3e0ff0cc876153254",
+    "url": "/boilerplate-react-devops/index.html"
+  },
+  {
+    "revision": "a21fc9fc625cf372110b",
+    "url": "/boilerplate-react-devops/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c25d640e449bf98f9fad",
+    "url": "/boilerplate-react-devops/static/js/2.17bbaca3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/boilerplate-react-devops/static/js/2.17bbaca3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a21fc9fc625cf372110b",
+    "url": "/boilerplate-react-devops/static/js/main.2defe0b4.chunk.js"
+  },
+  {
+    "revision": "91da4dc629bdeeff6fe3",
+    "url": "/boilerplate-react-devops/static/js/runtime-main.baca25c9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/boilerplate-react-devops/static/media/logo.5d5d9eef.svg"
+  }
+]);
